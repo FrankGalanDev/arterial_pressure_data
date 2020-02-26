84a5f6e09e0fc33dc8feba6c5f60813f87ddd4f3
Frank G <frankgalandev@gmail.com>
2020-02-26 19:48:53 +0100

    Add files via upload

---
+++ arterial_pressure analisys class.py
@@ -0,0 +1,41 @@
+
+import numpy as npy
+nombre = ""
+hora = ""
+sistole = ''
+diastole = ''
+dia = []
+sem = []
+periodo = []
+
+class MatrizPresion(object):
+  """ Clase que define como funciona"""
+  def __init__(self):
+    self.periodo = ""
+    
+    
+    def diario(self, datodia): 
+      x = self.datodia["nombre", "hora",]
+      dia = x
+      print (dia)
+      return dia
+
+    def semanal(self, datosemana):
+      a = self.datosemana.append(diario)
+      sem = a
+      print (sem)
+      return sem
+    
+  
+    def periodico(self, datoperiodo):  
+      b = self.datoperiodo.append(semanal)
+      per = b
+      print(per)
+      return per
+  
+    def analisis(self, analisis):
+      x = self.datoperiodo
+      ord = npy.array([x])# convertido el array a numpy podemos  realizar los calculos que deseemos
+      
+      return ord
+    print (ord)
